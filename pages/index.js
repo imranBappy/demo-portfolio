0b2ef34f17cdd0aee3a63800841a97a689b7103a
imranBappy <imranbappy.official@gmail.com>
2023-01-23 21:13:18 +0600

    project done

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ export default function Home() {
             </p>
           </div>
           <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10  flex-1">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
               <Image src={design} width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2  ">
                 Beautiful Designs
